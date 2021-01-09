64eaf17990b882d11e1e341ec9e8444af07af872
Juan David <gottygarcia@gmail.com>
2021-01-09 16:15:00 -0500

    sprite animation + react-spring animation card

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,31 @@
-import Head from 'next/head'
+import React from 'react'
 import styles from '../styles/Home.module.css'
+import { useSpring, animated } from 'react-spring'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Index = () => {
+	const [props, set] = useSpring(() => ({
+		xys: [0, 0, 1],
+		config: { mass: 5, tension: 350, friction: 40 },
+	}))
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+	const calc = (x, y) => [
+		-(y - window.innerHeight / 2) / 20,
+		(x - window.innerWidth / 2) / 20,
+		1.1,
+	]
+	const trans = (x, y, s) =>
+		`perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<animated.div
+				className={styles.card}
+				onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+				onMouseLeave={() => set({ xys: [0, 0, 1] })}
+				style={{ transform: props.xys.interpolate(trans) }}
+			/>
+		</div>
+	)
 }
+
+export default Index
